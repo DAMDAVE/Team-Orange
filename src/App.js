9af95d9f38ca561a8740d18edb1da5c81f7087fe
Omoyajowo David Oyindamola <omoyajowodavidoyindamola@gmail.com>
2024-09-13 14:34:50 +0100

    Backend Disconnected mode

--- src/App.js
+++ src/App.js
@@ -1,101 +1,74 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import { Routes, Route, useNavigate } from "react-router";
-import { FormProvider } from "./Components/Globals/FormContext";
 import tranquilLogo from "./Assets/home_logo.svg";
 import Login from "./Components/Forms/Login/Login";
 import SignIn from "./Components/Forms/SignIn/SignIn";
-import SignUp from "./Components/Forms/SignUp/SignUp";
 import Slides from "./Components/Forms/Slides/Slides";
 import Prompts from "./Components/Forms/NewUserPrompt/Prompt";
-import Redirector from "./Components/Forms/Redirect/Redirector";
-import Otp from "./Components/Forms/OTP/Otp";
-import Forgot from "./Components/Forms/ForgotPassword/Forgot";
 import Tranquil from "./Components/Tranquil App/Tranquil/Tranquil";
 import Profile from "./Components/Tranquil App/Profile/Profile/Profile";
-import EditProfile from "./Components/Tranquil App/Profile/Profile_Update/UpdateProfile";
+
 import { PageProvider } from "./Components/Tranquil App/Tranquil/PageContext";
-import Assessments from "./Components/Tranquil App/Home/AssessmentSchema/Assessment";
-import Results from "./Components/Tranquil App/Home/AssessmentResults/Results";
 import { useRef } from "react";
 import { preloadImages } from "./Components/Globals/Globals";
 
 function App() {
-    var navigate = useNavigate();
-    var textRef = useRef();
-    var [loaded, setLoaded] = useState(false);
-    function Opening() {
-        useEffect(() => {
-            setTimeout(() => {
-                textRef.current.innerText = "Find your inner Balance";
-            }, 1500);
+  var navigate = useNavigate();
+  var textRef = useRef();
+  var [loaded, setLoaded] = useState(false);
+  function Opening() {
+    useEffect(() => {
+      setTimeout(() => {
+        textRef.current.innerText = "Find your inner Balance";
+      }, 1500);
 
-            setTimeout(() => {
-                navigate("/login");
-            }, 4100);
-        });
+      setTimeout(() => {
+        navigate("/login");
+      }, 4100);
+    });
 
-        preloadImages([tranquilLogo]).then(() => {
-            setLoaded(true);
-        });
-        return (
-            <>
-                {" "}
-                {loaded ? (
-                    <div className="Opening">
-                        <div className="container">
-                            <header>
-                                <div className="imgText">
-                                    <div className="logo">
-                                        <img src={tranquilLogo} alt="" />
-                                    </div>
-                                    <p className="firstText" ref={textRef}>
-                                        Unleash the Power within
-                                    </p>
-                                </div>
-                            </header>
-                        </div>
-                    </div>
-                ) : null}
-            </>
-        );
-    }
-
+    preloadImages([tranquilLogo]).then(() => {
+      setLoaded(true);
+    });
     return (
-        <FormProvider>
-            <PageProvider>
-                <Routes>
-                    <Route index element={<Opening />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signIn" element={<SignIn />} />
-                    <Route path="/signUp" element={<SignUp />} />
-                    <Route path="/getStarted" element={<Slides />} />
-                    <Route path="/newUser" element={<Prompts />} />
-                    <Route
-                        path="/redirectPage/:token"
-                        element={<Redirector />}
-                    />
-                    <Route path="/otp" element={<Otp />} />
-                    <Route path="/forgotPassword" element={<Forgot />} />
-                    <Route path="/tranquil/*" element={<Tranquil />} />
-                    <Route path="/tranquil/profile/" element={<Profile />} />
-                    <Route
-                        path="/tranquil/profile/editProfile"
-                        element={<EditProfile />}
-                    />
-                    <Route
-                        path="/tranquil/assessments/:test/:specifics"
-                        element={<Assessments />}
-                    />
-                    <Route
-                        path="/tranquil/assessments/results/:test/:score"
-                        element={<Results />}
-                    />
-                </Routes>
-            </PageProvider>
-        </FormProvider>
-        //These are the routes to various pages of the app
+      <>
+        {" "}
+        {loaded ? (
+          <div className="Opening">
+            <div className="container">
+              <header>
+                <div className="imgText">
+                  <div className="logo">
+                    <img src={tranquilLogo} alt="" />
+                  </div>
+                  <p className="firstText" ref={textRef}>
+                    Unleash the Power within
+                  </p>
+                </div>
+              </header>
+            </div>
+          </div>
+        ) : null}
+      </>
     );
+  }
+
+  return (
+    <PageProvider>
+      <Routes>
+        <Route index element={<Opening />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/getStarted" element={<Slides />} />
+        <Route path="/newUser" element={<Prompts />} />
+        <Route path="/tranquil/*" element={<Tranquil />} />
+        <Route path="/tranquil/profile/" element={<Profile />} />
+      </Routes>
+    </PageProvider>
+
+    //These are the routes to various pages of the app
+  );
 }
 
 export default App;
--- src/Components/Tranquil App/Resources/Resources/Resources.jsx
+++ src/Components/Tranquil App/Resources/Resources/Resources.jsx
@@ -9,85 +9,78 @@ import { preloadImages } from "../../../Globals/Globals";
 import { useEffect, useState } from "react";
 import { PageDetails } from "../../Tranquil/PageContext";
 import Nav from "../../Tranquil/Nav/Nav";
-import { Route, Routes, useNavigate } from "react-router";
-import Articles from "../Articles/Articles";
+import { Route, Routes } from "react-router";
 
 function Initial() {
-    var [isImagesLoading, setImagesLoaded] = useState(false);
-    var { setCurrentPage } = PageDetails();
-    var navigate = useNavigate();
-    useEffect(() => {
-        setCurrentPage("resources");
-    });
-    useEffect(() => {
-        const imagesToPreload = [backwards, Article, Videos, Materials, Listen];
-        preloadImages(imagesToPreload)
-            .then(() => {
-                const imageTimer = setTimeout(() => {
-                    setImagesLoaded(true);
-                }, 1000);
+  var [isImagesLoading, setImagesLoaded] = useState(false);
+  var { setCurrentPage } = PageDetails();
 
-                return () => {
-                    clearTimeout(imageTimer);
-                };
-            })
-            .catch(() => {
-                console.log("Error Loading Images");
-            });
-    }, []);
+  useEffect(() => {
+    setCurrentPage("resources");
+  });
+  useEffect(() => {
+    const imagesToPreload = [backwards, Article, Videos, Materials, Listen];
+    preloadImages(imagesToPreload)
+      .then(() => {
+        const imageTimer = setTimeout(() => {
+          setImagesLoaded(true);
+        }, 1000);
 
-    return (
-        <div className="ResourcesPage">
-            {isImagesLoading === false ? <Spinner /> : null}
+        return () => {
+          clearTimeout(imageTimer);
+        };
+      })
+      .catch(() => {
+        console.log("Error Loading Images");
+      });
+  }, []);
 
-            <Nav link="/tranquil/home" />
-            <header>
-                <h1>Resources</h1>
-            </header>
-            <div className="sections">
-                <div
-                    className="group"
-                    onClick={() => {
-                        navigate("articles");
-                    }}
-                >
-                    <div>
-                        <img src={Article} alt="" />
-                    </div>
-                    <p>Articles</p>
-                </div>
+  return (
+    <div className="ResourcesPage">
+      {isImagesLoading === false ? <Spinner /> : null}
 
-                <div className="group">
-                    <div>
-                        <img src={Videos} alt="" />
-                    </div>
-                    <p>Videos</p>
-                </div>
-
-                <div className="group">
-                    <div>
-                        <img src={Materials} alt="" />
-                    </div>
-                    <p>Materials</p>
-                </div>
-
-                <div className="group">
-                    <div>
-                        <img src={Listen} alt="" />
-                    </div>
-                    <p>Listen</p>
-                </div>
-            </div>
+      <Nav link="/tranquil/home" />
+      <header>
+        <h1>Resources</h1>
+      </header>
+      <div className="sections">
+        <div className="group">
+          <div>
+            <img src={Article} alt="" />
+          </div>
+          <p>Articles</p>
         </div>
-    );
+
+        <div className="group">
+          <div>
+            <img src={Videos} alt="" />
+          </div>
+          <p>Videos</p>
+        </div>
+
+        <div className="group">
+          <div>
+            <img src={Materials} alt="" />
+          </div>
+          <p>Materials</p>
+        </div>
+
+        <div className="group">
+          <div>
+            <img src={Listen} alt="" />
+          </div>
+          <p>Listen</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function Resources() {
-    return (
-        <Routes>
-            <Route path="" element={<Initial />} />
-            <Route path="articles" element={<Articles />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="" element={<Initial />} />
+    </Routes>
+  );
 }
 export default Resources;
